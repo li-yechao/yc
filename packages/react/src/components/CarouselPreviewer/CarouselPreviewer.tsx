c94d1fcfe91ebda4234511e61c55ed68014df40d
Yechao Li <liyechao@yechao.xyz>
2023-03-17 15:05:40 +0800

    feat: support video and audio & allow custom render item

--- packages/react/src/components/CarouselPreviewer/CarouselPreviewer.tsx
+++ packages/react/src/components/CarouselPreviewer/CarouselPreviewer.tsx
@@ -4,18 +4,26 @@ import 'swiper/css/navigation'
 
 import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material'
 import { Box, BoxProps, IconButton, styled } from '@mui/material'
-import { useEffect, useRef } from 'react'
+import { ReactNode, useEffect, useRef } from 'react'
 import { EffectCoverflow, Mousewheel, Navigation } from 'swiper'
 import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
 
 export interface CarouselPreviewerProps extends Pick<BoxProps, 'sx' | 'className'> {
   index?: number
   onIndexChange?: (index: number) => void
-  data: { url: string }[]
+  data: { key?: string | number; url: string; type?: 'img' | 'video' | 'audio' }[]
   effect?: 'coverflow'
+  renderItem?: (data: CarouselPreviewerProps['data'][0]) => ReactNode
 }
 
-export default function CarouselPreviewer({ index, data, effect, onIndexChange, ...props }: CarouselPreviewerProps) {
+export default function CarouselPreviewer({
+  index,
+  data,
+  effect,
+  onIndexChange,
+  renderItem,
+  ...props
+}: CarouselPreviewerProps) {
   const swiper = useRef<SwiperRef>(null)
 
   const prev = useRef<HTMLDivElement>(null)
@@ -52,12 +60,8 @@ export default function CarouselPreviewer({ index, data, effect, onIndexChange, 
       }}
     >
       {data.map(item => (
-        <_SwiperSlide key={item.url}>
-          <Box
-            component="img"
-            src={item.url}
-            sx={{ display: 'inline-block', maxWidth: '100%', height: '100%', borderRadius: 1 }}
-          />
+        <_SwiperSlide key={item.key || item.url}>
+          {renderItem ? renderItem(item) : <ItemRenderer {...item} />}
         </_SwiperSlide>
       ))}
 
@@ -72,6 +76,26 @@ export default function CarouselPreviewer({ index, data, effect, onIndexChange, 
         </IconButton>
       </Box>
     </_Swiper>
+  )
+}
+
+function ItemRenderer({ url, type }: CarouselPreviewerProps['data'][0]) {
+  return type && ['video', 'audio'].includes(type) ? (
+    <Box sx={{ display: 'inline-flex', maxWidth: '100%', height: '100%', alignItems: 'center' }}>
+      <Box
+        component={type}
+        src={url}
+        controls
+        sx={{
+          display: 'block',
+          maxWidth: '100%',
+          height: type === 'video' ? '100%' : undefined,
+          borderRadius: 1,
+        }}
+      />
+    </Box>
+  ) : (
+    <Box component="img" src={url} sx={{ display: 'block', maxWidth: '100%', height: '100%', borderRadius: 1 }} />
   )
 }
 
